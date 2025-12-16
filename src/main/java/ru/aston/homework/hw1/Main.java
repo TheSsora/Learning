81831f2fb188b67a3c4e19666397415be4c93f26
Ssora <lemiomor@gmail.com>
2025-12-16 13:05:31 +0300

    init2

--- src/main/java/ru/aston/homework/hw1/Main.java
+++ src/main/java/ru/aston/homework/hw1/Main.java
@@ -1,7 +1,5 @@
 package ru.aston.homework.hw1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Immutable immutable = new Immutable(new Mutable("NewMutable"));
