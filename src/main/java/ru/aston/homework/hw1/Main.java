f780065f9e97fdac2830d431a6bd3a0abb101240
Ssora <lemiomor@gmail.com>
2025-12-16 13:05:05 +0300

    hw2_init

--- src/main/java/ru/aston/homework/hw1/Main.java
+++ src/main/java/ru/aston/homework/hw1/Main.java
@@ -1,7 +1,4 @@
-package ru.aston.homework;
-
-import ru.aston.homework.hw1.Immutable;
-import ru.aston.homework.hw1.Mutable;
+package ru.aston.homework.hw1;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
