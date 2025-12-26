2e52b1ee9776233a7b662a3fa139bf853d7e7611
Ssora <lemiomor@gmail.com>
2025-12-26 15:10:11 +0300

    CoR pattern

---
+++ src/main/java/ru/aston/homework/hw3/CoR/Approver.java
@@ -0,0 +1,11 @@
+package ru.aston.homework.hw3.CoR;
+
+public abstract class Approver {
+    protected Approver nextApprover;
+
+    public void setNextApprover(Approver nextApprover) {
+        this.nextApprover = nextApprover;
+    }
+
+    public abstract void processRequest(PurchaseRequest request);
+}
---
+++ src/main/java/ru/aston/homework/hw3/CoR/Main.java
@@ -0,0 +1,26 @@
+package ru.aston.homework.hw3.CoR;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Approver manager = new Manager();
+        Approver director = new Director();
+        Approver ceo = new CEO();
+
+        manager.setNextApprover(director);
+        director.setNextApprover(ceo);
+
+        PurchaseRequest smallPurchase = new PurchaseRequest(500, "Канцелярия");
+        PurchaseRequest mediumPurchase = new PurchaseRequest(5000, "Ноутбук");
+        PurchaseRequest largePurchase = new PurchaseRequest(50000, "Сервер");
+
+        System.out.println("--- Запрос на 500 руб ---");
+        manager.processRequest(smallPurchase);
+
+        System.out.println("\n--- Запрос на 5000 руб ---");
+        manager.processRequest(mediumPurchase);
+
+        System.out.println("\n--- Запрос на 50000 руб ---");
+        manager.processRequest(largePurchase);
+    }
+}
