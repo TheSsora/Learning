f7c47481b92763d53cd6750c2a8315ba5115420b
Ssora <lemiomor@gmail.com>
2025-12-26 20:26:44 +0300

    Decorator pattern

---
+++ src/main/java/ru/aston/homework/hw3/Decorator/NotifierDecorator.java
@@ -0,0 +1,14 @@
+package ru.aston.homework.hw3.Decorator;
+
+public class NotifierDecorator implements Notifier{
+    protected Notifier wrappedNotifier;
+
+    public NotifierDecorator(Notifier notifier) {
+        this.wrappedNotifier = notifier;
+    }
+
+    @Override
+    public void send(String message) {
+        wrappedNotifier.send(message);
+    }
+}
